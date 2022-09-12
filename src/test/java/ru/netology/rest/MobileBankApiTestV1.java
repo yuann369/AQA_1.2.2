8ef46afb6b87f69505e14d91518781178ea4dc64
Anna Yurasova <anna.yurasova@ssi-ru.local>
2022-09-12 20:53:33 +0400

    add wrong test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,22 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-class MobileBankApiTestV1 {
+class PostmanEchoTest {
     @Test
-    void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+    void forwardedPort() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("headers.x-forwarded-port", equalTo("447"))
+        ;
     }
 }
